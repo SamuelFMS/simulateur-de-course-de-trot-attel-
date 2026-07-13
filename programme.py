166c88c91bc3339ddc5252777e78de4fb90626b3
Samuel Curran <samuel.curran@fms-ea.com>
2026-07-13 13:09:15 +0200

    Notion de tour

--- programme.py
+++ programme.py
@@ -1,3 +1,5 @@
+import utils
+
 if __name__ == '__main__':
     speedPerDiceRoll = {0: [0,1,1,1,2,2],
                         1: [0,0,1,1,1,2],
@@ -6,4 +8,26 @@ if __name__ == '__main__':
                         4: [-1,0,0,0,1,1],
                         5: [-2,-1,0,0,0,1],
                         6:[-2,-1,0,0,0,"DQ"],}
+
     speed_horse = [0,23,46,69,92,115,138]
+    numberOfHorse = 5
+    current_speed_horse = [0]*numberOfHorse
+    current_distance_horse = [0]*numberOfHorse
+
+    while(True):
+        #Effectuer un tour
+        for x in range(numberOfHorse):
+            if current_speed_horse[x] == "DQ":
+                continue
+            resultatDe = utils.rollADiche()
+            currentSpeed = current_speed_horse[x]
+            if speedPerDiceRoll[currentSpeed][resultatDe-1] == "DQ":
+                current_speed_horse[x] = "DQ"
+                continue
+            newSpeed = currentSpeed + speedPerDiceRoll[currentSpeed][resultatDe-1]
+            current_speed_horse[x] = newSpeed
+            currentDistance = current_distance_horse[x] + speed_horse[newSpeed]
+            current_distance_horse[x] = currentDistance
+        print(current_speed_horse)
+        print(current_distance_horse)
+        input("Next? ")
