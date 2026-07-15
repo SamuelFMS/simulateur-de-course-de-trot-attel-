3f5184b772f8e86b3e83d46ac570eef33a2d5f08
Samuel Curran <samuel.curran@fms-ea.com>
2026-07-15 13:22:14 +0200

    Ajout de commentaire pour utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,21 @@
 import random
 
+"""
+    methode permettant d'avoir un nombre entier aléatoire entre min et max
+    :param min
+    :param max
+    :return int
+"""
 def rollADiche(min = 1, max = 6):
     return random.randint(min, max)
 
+
+"""
+    methode permetant la saisie d'un nombre attends un numero entier positif ou négatif
+    :param message (La chaine a afficher a l'utilisateur pour la saisie)
+    :param errorMessage (La chaine a afficher lors d'une erreur de saisie)
+    :return retourne le nombre saisie par l'utilisateur
+"""
 def askNumber(message = None, erroMessage = None):
     while True:
         number = input("Entrer un numero: " if message is None else message)
@@ -11,6 +24,13 @@ def askNumber(message = None, erroMessage = None):
         else:
             print("Incorrect input" if erroMessage is None else erroMessage)
 
+"""
+    méthode permetant la saisie avec une méthode passer en parametre pour verifier la saisie
+    :param Condition (méthode permettant de verifier si la saisie est correcte)
+    :param message (La chaine a afficher a l'utilisateur pour la saisie)
+    :param erroMessage (La chaine a afficher lors dune erreur de saisie)
+    :return saisi (L'input)
+"""
 def askCondition(Condition, message = None, erroMessage = None):
     while True:
         saisi = input("Entrer: " if message is None else message)
@@ -19,6 +39,12 @@ def askCondition(Condition, message = None, erroMessage = None):
         else:
             print("Incorrect input" if erroMessage is None else erroMessage)
 
+
+"""
+    méthode permettant la verification d'une chaine en nombre
+    :param chaine (la chaine que l'on veut verifier)
+    :return boolean
+"""
 def isANumber(chaine):
     if chaine.lstrip("-").isdigit():
         return True
